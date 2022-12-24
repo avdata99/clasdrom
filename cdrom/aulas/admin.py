54017825d627317142259b191812858cca1de0ae
avdata99 <andres@data99.com.ar>
2022-12-24 02:55:22 -0300

    Aulas

--- cdrom/aulas/admin.py
+++ cdrom/aulas/admin.py
@@ -4,7 +4,7 @@ from aulas.models import Aula
 
 @admin.register(Aula)
 class AulaAdmin(admin.ModelAdmin):
-    list_display = ("codigo_pais", "codigo_area", "numero")
-    list_filter = ("codigo_area",)
-    search_fields = ("numero",)
-    ordering = ("codigo_area", "numero")
+    list_display = ("institucion", "nombre", "capacidad_alumnos")
+    list_filter = ("institucion",)
+    search_fields = ("nombre",)
+    ordering = ("nombre",)
--- cdrom/aulas/models.py
+++ cdrom/aulas/models.py
@@ -5,7 +5,7 @@ class Aula(models.Model):
     institucion = models.ForeignKey("instituciones.Institucion", on_delete=models.SET_NULL, null=True, blank=True)
     nombre = models.CharField(max_length=100)
     # TODO las aulas podrían tener diferentes configuraciones (con o sin mesas, etc que cambiarían su capacidad)
-    capacidad_alumnos = models.IntegerField()
+    capacidad_alumnos = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.nombre
