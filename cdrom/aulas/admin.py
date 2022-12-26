a80c5a5856f32c2f053ec7975ce7a2cf2e58f9ba
avdata99 <andres@data99.com.ar>
2022-12-26 14:35:04 -0300

    Media y statics

--- cdrom/aulas/admin.py
+++ cdrom/aulas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from aulas.models import Aula
+from aulas.models import Aula, FotoAula
 
 
 @admin.register(Aula)
@@ -8,3 +8,10 @@ class AulaAdmin(admin.ModelAdmin):
     list_filter = ("institucion",)
     search_fields = ("nombre",)
     ordering = ("nombre",)
+
+
+@admin.register(FotoAula)
+class FotoAulaAdmin(admin.ModelAdmin):
+    list_display = ("aula", "descripcion", "orden")
+    list_filter = ("aula",)
+    ordering = ("orden",)
--- cdrom/aulas/models.py
+++ cdrom/aulas/models.py
@@ -4,11 +4,29 @@ from django.db import models
 class Aula(models.Model):
     institucion = models.ForeignKey("instituciones.Institucion", on_delete=models.SET_NULL, null=True, blank=True)
     nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True, blank=True)
     # TODO las aulas podrían tener diferentes configuraciones (con o sin mesas, etc que cambiarían su capacidad)
     capacidad_alumnos = models.IntegerField(null=True, blank=True)
     ac = models.BooleanField(default=False)
     proyector = models.BooleanField(default=False)
     pizarra = models.BooleanField(default=False)
 
+    def primera_foto(self):
+        if self.fotos.count() > 0:
+            return self.fotos.first()
+
     def __str__(self):
         return self.nombre
+
+
+class FotoAula(models.Model):
+    aula = models.ForeignKey(Aula, on_delete=models.CASCADE, related_name="fotos")
+    foto = models.ImageField(upload_to="imgs/aulas")
+    orden = models.IntegerField(default=100)
+    descripcion = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.aula.nombre
+
+    class Meta:
+        ordering = ["orden"]
