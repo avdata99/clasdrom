ab6f3673c05ec6e294c1488fd1e05c50145ccb4e
german <germankay@gmail.com>
2023-07-26 10:25:36 -0300

    cambiando base.html  por home.html

--- cdrom/core/views.py
+++ cdrom/core/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 
 class HomePage(TemplateView):
-    template_name = 'base.html'
+    template_name = 'home.html'
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
