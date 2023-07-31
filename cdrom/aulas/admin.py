87af1529a038aa7523ff6a2775e34fc5075c8b00
avdata99 <andres@data99.com.ar>
2023-07-31 17:47:14 -0300

    Agregar admins faltantes

--- cdrom/aulas/admin.py
+++ cdrom/aulas/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from aulas.models import Aula, FotoAula
+from aulas.models import (
+    Aula,
+    CaracteristicaAula,
+    CaracteristicaEnAula,
+    FotoAula,
+)
 
 
 @admin.register(Aula)
@@ -13,3 +18,14 @@ class AulaAdmin(admin.ModelAdmin):
 class FotoAulaAdmin(admin.ModelAdmin):
     list_display = ("aula", "descripcion", "orden")
     list_filter = ("aula", "aula__institucion")
+
+
+@admin.register(CaracteristicaAula)
+class CaracteristicaAulaAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+
+
+@admin.register(CaracteristicaEnAula)
+class CaracteristicaEnAulaAdmin(admin.ModelAdmin):
+    list_display = ("aula", "caracteristica", "disponible", "se_debe_pedir")
+    list_filter = ("aula", "caracteristica")
