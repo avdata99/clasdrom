c6f58f0c59c89587ab048d6138a949d633fa766e
german <germankay@gmail.com>
2023-08-09 17:23:43 -0300

    menu del contextprosesor

--- cdrom/core/context_processors.py
+++ cdrom/core/context_processors.py
@@ -59,6 +59,26 @@ def app_base_context(request):
                         'titulo': 'Crear Curso',
                         'active': False,
                         'link': reverse('curso_add'),
+                    },
+                },
+            },
+
+            'mlist2': {
+                'titulo': 'Instituciones',
+                'active': True,
+                'subitems': {
+                    'm1': {
+                        'titulo': 'Listar Instituciones',
+                        'active': False,
+                        'link': reverse('institucion_list'),
+                    },
+                    'm2': {
+                        'titulo': '-',
+                    },
+                    'm3': {
+                        'titulo': 'Crear Institucion',
+                        'active': False,
+                        'link': reverse('institucion_add'),
                     }
                 }
             }
