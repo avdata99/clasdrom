8ac24c33c9f66739fc4e18797ede8ffbd6db2aa3
german <germankay@gmail.com>
2023-08-15 09:43:03 -0300

    correcciones finales

--- cdrom/instituciones/views.py
+++ cdrom/instituciones/views.py
@@ -33,41 +33,28 @@ class InstitucionCreateView(LoginRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print("contex 001", context)
         if self.request.POST:
             context['form'] = InstitucionForm(self.request.POST, self.request.FILES)
-            print("context1 002 ", context, self.request.POST, self.request.FILES)
             context['formset_fotos'] = FotoInstitucionFormSet(self.request.POST, self.request.FILES)
-            print("context2 003", context, self.request.POST, self.request.FILES)
 
         else:
             context['form'] = InstitucionForm()
-            print("context1 004:", context)
             context['formset_fotos'] = FotoInstitucionFormSet()
-            print("context2 005:", context)
         return context
 
     def form_valid(self, form):
         context = self.get_context_data()
-        print("context from valid:", context)
         formset_fotos = context['formset_fotos']
-        print("Form is valid 006:", form.is_valid())
-        print("Formset is valid 007:", formset_fotos.is_valid())
 
         if form.is_valid() and formset_fotos.is_valid():
             self.object = form.save()
-            print("Form saved 008")
             self.object.site = Site.objects.get_current()
             formset_fotos.instance = self.object
             formset_fotos.save()
-            print("Formset saved 009")
             return super().form_valid(form)
         else:
-            print("Form errors 010:", form.errors)
-            print("Formset errors 011", formset_fotos.errors)
             return super().form_invalid(form)
 
     def get_success_url(self, **kwargs):
         success_url = reverse('institucion_list')
-        print(f'Redirect to {success_url}')
         return success_url
