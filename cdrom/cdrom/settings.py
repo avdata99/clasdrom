c1bc9b51f1ae264b6eed8d203ffe61521d676423
avdata99 <andres@data99.com.ar>
2025-08-10 21:11:45 -0300

    UI

--- cdrom/cdrom/settings.py
+++ cdrom/cdrom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-_9_vc(b)j*k-b*_g$-oyd3$s1mx=mpo4b==mt_oak9gre5%ftw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
