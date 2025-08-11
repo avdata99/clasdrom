020e3ef34128f4917c176d59f87a5c10f8bd5c47
avdata99 <andres@data99.com.ar>
2025-08-10 22:26:26 -0300

    tpl curso inicial

--- cdrom/cursos/urls.py
+++ cdrom/cursos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from cursos.views import CursoDetailView, CursoListView, CursoCreateView
+from cursos.views import CursoDetailView, CursoStaticDetailView, CursoListView  # , CursoCreateView
 
 
 urlpatterns = [
-    path('lista/', CursoListView.as_view(), name='curso_list'),
-    path('<int:pk>/', CursoDetailView.as_view(), name='curso_detail'),
-    path('add/', CursoCreateView.as_view(), name='curso_add'),
+    path('/', CursoListView.as_view(), name='curso_list'),
+    path('/<str:curso_name>/', CursoDetailView.as_view(), name='curso_detail'),
+    path('/s/<str:curso_name>/', CursoStaticDetailView.as_view(), name='curso_static_detail'),
+    # path('add/', CursoCreateView.as_view(), name='curso_add'),
 ]
--- cdrom/cursos/views.py
+++ cdrom/cursos/views.py
@@ -1,4 +1,7 @@
-from django.views.generic import DetailView, ListView, CreateView
+from django.views.generic import DetailView, ListView, CreateView, TemplateView
+from django.http import Http404
+from django.template.loader import get_template
+from django.template.exceptions import TemplateDoesNotExist
 from cursos.models import Curso
 from django.urls import reverse_lazy
 from cursos.form import CursoForm
@@ -14,11 +17,32 @@ class CursoDetailView(DetailView):
 
     model = Curso
     context_object_name = "curso"
+    template_name = "cursos/curso_detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['site_brand'] = self.object.titulo
         return context
+
+
+class CursoStaticDetailView(TemplateView):
+    """ This is a static site for a course.
+    We have the kwarg 'curso_name' to identify the course to define the template.
+    """
+
+    def get_template_names(self):
+        curso_name = self.kwargs.get('curso_name')
+        if not curso_name:
+            raise Http404("Curso no definido")
+
+        # Check if the template exists using a simpler approach
+        template = f'cursos/s/{curso_name}.html'
+        try:
+            get_template(template)
+        except TemplateDoesNotExist:
+            raise Http404("Curso no encontrado")
+
+        return [template]
 
 
 class CursoCreateView(CreateView):
